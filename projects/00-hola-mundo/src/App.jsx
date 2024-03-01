bf75085fcdbee0c4eb363e0680cfa94e1125dfd9
Carlos <carlosdanieltc@gmail.com>
2024-03-01 14:25:46 +0100

    Intro useState

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,14 +1,26 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
+import { useState } from 'react'
 
 export function App(){
     const formatUserName = (userName) => `@${userName}`
     return (
         <section className='App'>
-            <TwitterFollowCard formatUserName={formatUserName} isFollowing userName="carlosdanieltc" name="Carlos Tabares"></TwitterFollowCard> 
-            <TwitterFollowCard formatUserName={formatUserName} isFollowing={false} userName="JoeBiden" name="Joe Biden"></TwitterFollowCard> 
-            <TwitterFollowCard formatUserName={formatUserName} isFollowing userName="DonaldJTrump" name="Donald Trump"></TwitterFollowCard>
-            <TwitterFollowCard formatUserName={formatUserName} isFollowing userName="BarackObama" name="Barack Obama"></TwitterFollowCard>
+            <TwitterFollowCard formatUserName={formatUserName} isFollowing userName="carlosdanieltc">
+                Carlos Tabares
+            </TwitterFollowCard> 
+
+            <TwitterFollowCard formatUserName={formatUserName} isFollowing={false} userName="JoeBiden">
+                Joe Biden
+            </TwitterFollowCard> 
+
+            <TwitterFollowCard formatUserName={formatUserName} isFollowing userName="DonaldJTrump">
+                Donald Trump
+            </TwitterFollowCard>
+
+            <TwitterFollowCard formatUserName={formatUserName} isFollowing userName="BarackObama">
+                Barack Obama
+            </TwitterFollowCard>
         </section>
     )
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,16 +1,27 @@
-export function TwitterFollowCard({formatUserName, userName, name, isFollowing}){
+import { useState } from "react"
+
+export function TwitterFollowCard({children, formatUserName, userName = 'unknown'}){
+    const [isFollowing, setIsFollowing] = useState(false)
+
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+
+    const handleClick = () =>{
+        setIsFollowing(!isFollowing)
+    }
+
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
                 <img src={`https://unavatar.io/${userName}`} alt="Imagen" className='tw-followCard-avatar'/>
                 <div className='tw-followCard-info'>
-                    <strong>{name}</strong>
+                    <strong>{children}</strong>
                     <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
                 </div>
             </header>
 
             <aside>
-                <button className='tw-followCard-button'>Seguir</button>
+                <button className={buttonClassName} onClick={handleClick}>{text}</button>
             </aside>
         </article>
     )
