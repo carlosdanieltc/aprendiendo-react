25a7c7b0a6c34a192d022f6731eaf3cfb52a199b
Carlos <carlosdanieltc@gmail.com>
2024-03-06 13:40:09 +0100

    2 Clase, TicTacToe Funcionamiento básico

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -8,19 +8,68 @@ const TURNS = {
 
 const Square = ({children, isSelected, updateBoard, index}) => {
   const className = `square ${isSelected ? 'is-selected': ''}`
+
+  const handleClick= () => {
+    updateBoard(index);
+  }
+
   return (
-    <div className={className}>
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
 
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null)) 
+
   const [turn, setTurn] = useState(TURNS.X)
-  const updateBoard = () => {
-    
+
+  const [winner, setWinner] = useState(null) //null es que no hay ganador y false y es que hay un empate
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS){
+      const [a,b,c] = combo
+      if(
+        boardToCheck[a] && 
+        boardToCheck[a] === boardToCheck[b] && //
+        boardToCheck[a] === boardToCheck[c]
+      ){
+        return boardToCheck[a]
+      }
+    }
+    return null; // Si no hay ganador
   }
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return //Valida para evitar actualizar el board
+    //si se hace click en un sitio donde ya se pulsó o si hay ganador
+
+    const newBoard = [...board]
+    newBoard[index] = turn // X u O
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    //revisar si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
+  }
+
   return  (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
@@ -32,8 +81,8 @@ function App() {
               key={index}
               index={index}
               updateBoard={updateBoard}
-              >  
-                           
+              >     
+              {board[index]}                       
               </Square>
             )
           })
