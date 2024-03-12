38356229bfc69a801e6977b54b1e230c05e76e69
Carlos <carlosdanieltc@gmail.com>
2024-03-12 18:22:51 +0100

    3. Inicio comportamiento base MouseFollower

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [enabled,setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x:0 , y:0 })
+
+  useEffect(() => {
+
+    console.log(enabled)
+
+    const handleMove = (event) => {
+      const {clientX, clientY} = event
+      setPosition({ x:clientX, y:clientY })
+
+      console.log('handleMove',{clientX, clientY})
+    }
+
+
+
+    if (enabled) {
+      window.addEventListener('pointermove',handleMove)
+    }
+
+  },[enabled])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        //transform: 'translate(0px, 0px)'
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
   )
 }
 
