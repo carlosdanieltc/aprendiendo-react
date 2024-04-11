d91473b75e7ddc324738d4ecfa1d14b3804533dd
Carlos Tabares <carlosdanieltc@gmail.com>
2024-04-11 09:55:29 +0200

    4. Añadidos estilos básicos y separados effects

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 //const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${FirstWord}?size=50&color=red&json=true`
@@ -9,24 +10,31 @@ export function App(){
     const [fact, setFact] = useState()
     const [imageUrl, setImageUrl] = useState()
 
+    //Efecto para recuperar la cita al cargar la página
     useEffect(() => {
         fetch(CAT_ENDPOINT_RANDOM_FACT)
             .then(res => res.json())
             .then(data => {
                 const {fact} = data
                 setFact(fact)
-
-                const firstWord = fact.split(' ')[0]
-                const threeFirstWords = fact.split(' ',3).join(' ')
-                fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red`)
-                    .then(res => res)
-                    .then(response => {
-                        const {url} = response
-                        setImageUrl(url)
-                    })
             })
     }, [])
+
+    //Efecto para recuperar la imagen cada vez que tenemos una cita nueva
+    useEffect(() => {
+        if (!fact) return
+
+        //const firstWord = fact.split(' ')[0]
+        const threeFirstWords = fact.split(' ',3).join(' ')
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red`)
+            .then(res => res)
+            .then(response => {
+                const {url} = response
+                setImageUrl(url)
+            })
+    },[fact])
     
+
     return (
         <main>
             <h1>TituGat</h1>
