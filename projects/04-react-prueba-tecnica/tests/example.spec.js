4e291a7443d82badd25863da096d40e3596f8f63
Carlos Tabares <carlosdanieltc@gmail.com>
2024-04-15 11:08:29 +0200

    4. Cambios Test

--- projects/04-react-prueba-tecnica/tests/example.spec.js
+++ projects/04-react-prueba-tecnica/tests/example.spec.js
@@ -1,11 +1,11 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
-const LOCALHOST_URL = 'https://localhost:5173/'
+const LOCALHOST_URL = 'http://localhost:5173/'
 
 test('app shows random fact and image', async ({ page }) => {
-  await page.goto(LOCALHOST_URL);
+  await page.goto(LOCALHOST_URL)
 
   const text = await page.getByRole('paragraph')
   const image = await page.getByRole('img')
@@ -15,5 +15,4 @@ test('app shows random fact and image', async ({ page }) => {
 
   await expect(textContent?.length).toBeGreaterThan(0)
   await expect(imageSrc?.startsWith(CAT_PREFIX_IMAGE_URL)).toBeTruthy()
-
-});
+})
