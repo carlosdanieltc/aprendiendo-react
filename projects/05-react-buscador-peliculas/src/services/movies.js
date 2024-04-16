fc7fa029238e4f03afca6fe938140befa3889b5b
Carlos Tabares <carlosdanieltc@gmail.com>
2024-04-16 13:56:54 +0200

    5. Servicio movies

---
+++ projects/05-react-buscador-peliculas/src/services/movies.js
@@ -0,0 +1,30 @@
+const API_KEY= '36364b7f'
+
+export const searchMovies = async ({search}) => {
+    if (search === '') return null
+
+    try {
+        const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
+        const json = await response.json()
+
+        const movies = responseMovies.Search
+
+        const mappedMovies = movies?.map(movie => ({
+            id: movie.imdbID,
+            title: movie.Title,
+            year: movie.Year,
+            poster: movie.Poster
+        }))
+    }catch(e){
+        throw new Error('Error searching movies')
+    }
+}
+
+// if (search) {
+//     //setResponsiveMovies(withResults)
+//     return fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
+//       .then(res => res.json())
+//       .then(json => setResponsiveMovies(json))
+//   } else {
+//     return setResponsiveMovies(withoutResults)
+//   }
