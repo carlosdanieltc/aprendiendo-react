ca6e0fcac7d46da7cc826c1312c2a79852a57848
Carlos Tabares <carlosdanieltc@gmail.com>
2024-04-18 13:12:38 +0200

    6. Filtrado de productos por precio y categoria

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -7,21 +7,29 @@ function App() {
 
   const [products] = useState(initialProducts)
 
-  // const [filters, setFilters] = useState({
-  //   category: 'all',
-  //   minPrice: 0
-  // })
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  })
 
-  // const filterProducts = (products) => {
-  //   return products.filter(product => {
-  //     return 
-  //   })
-  // }
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return (
+        product.price >= filters.minPrice &&
+        (
+          filters.category == 'all' || 
+          product.category == filters.category
+        )
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
 
   return (
     <>
       <h1>Shopping cart 🛒</h1>
-      <Products products={products}/>
+      <Products products={filteredProducts}/>
     </>
   )
 }
