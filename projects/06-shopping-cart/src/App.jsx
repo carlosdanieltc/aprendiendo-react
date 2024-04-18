8f5d27a9110c2315155f05c5d0c578f0a30b6c0b
Carlos Tabares <carlosdanieltc@gmail.com>
2024-04-18 13:02:09 +0200

    6. Mostrando lista de productos

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react"
+import { Products } from "./components/Products"
+import { products as initialProducts} from './mocks/products'
+
 
 function App() {
+
+  const [products] = useState(initialProducts)
+
+  // const [filters, setFilters] = useState({
+  //   category: 'all',
+  //   minPrice: 0
+  // })
+
+  // const filterProducts = (products) => {
+  //   return products.filter(product => {
+  //     return 
+  //   })
+  // }
+
   return (
-    <h1>Shopping cart 🛒</h1>
+    <>
+      <h1>Shopping cart 🛒</h1>
+      <Products products={products}/>
+    </>
   )
 }
 
