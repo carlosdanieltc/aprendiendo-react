4cbae4a632e611ec7ba1a4b451680baacef02761
Carlos Tabares <carlosdanieltc@gmail.com>
2024-04-19 11:03:17 +0200

    6. Utilización de useId

--- projects/06-shopping-cart/src/components/Filters.jsx
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import { useState, useId } from 'react'
 import './Filters.css'
 
 export function Filters ({onChange}){
 
     const [minPrice, setMinPrice] = useState(0)
+    const minPriceFilterId = useId()
+    const cayegoryFilterId = useId()
 
     const handleChangeMinPrice = (event) => {
         //POR REVISAR
@@ -25,10 +27,10 @@ export function Filters ({onChange}){
     return (
         <section className="filters">
             <div>
-                <label htmlFor="price">Precio mínimo:</label>
+                <label htmlFor={minPriceFilterId}>Precio mínimo:</label>
                 <input 
                     type="range"
-                    id="price"
+                    id={minPriceFilterId}
                     min='0'
                     max='1000' 
                     onChange={handleChangeMinPrice}
@@ -37,8 +39,8 @@ export function Filters ({onChange}){
             </div>
 
             <div>
-                <label htmlFor="category">Categoría</label>
-                <select id="category" onChange={handleChangeCategory}>
+                <label htmlFor={cayegoryFilterId}>Categoría</label>
+                <select id={cayegoryFilterId} onChange={handleChangeCategory}>
                     <option value="all">Todas</option>
                     <option value="laptops">Portátiles</option>
                     <option value="smartphones">Móviles</option>
