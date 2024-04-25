7fa561b864fc91435a3865da4de0ca00ebeecf9d
Carlos Tabares <carlosdanieltc@gmail.com>
2024-04-25 12:56:06 +0200

    6. Separación en el useCartReducer

--- projects/06-shopping-cart/src/context/cart.jsx
+++ projects/06-shopping-cart/src/context/cart.jsx
@@ -1,11 +1,10 @@
-import { createContext,useReducer,useState } from "react";
+import { createContext,useReducer } from "react";
 import { cartInitialState, cartReducer } from "../reducers/cart";
 
 //1. Crear el contexto
 export const CartContext = createContext()
 
-//2. Crear provider
-export function CartProvider ({children}){
+function useCartReducer(){
     const [state, dispatch] =  useReducer(cartReducer, cartInitialState)
 
     const addToCart = product => dispatch({
@@ -19,6 +18,14 @@ export function CartProvider ({children}){
     })
 
     const clearCart = () => dispatch({type: 'CLEAR_CART'})
+
+    return {state, addToCart, removeFromCart, clearCart}
+}
+
+//2. Crear provider
+export function CartProvider ({children}){
+    
+    const {state, addToCart, removeFromCart, clearCart} = useCartReducer()
 
     return (
         <CartContext.Provider value = {{
