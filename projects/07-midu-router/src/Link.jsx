939445db4bbdcf62bf9d88b834dba60018f525cf
Carlos Tabares <carlosdanieltc@gmail.com>
2024-04-29 13:32:43 +0200

    7. Cambios en Link para que funcione correctamente con los comandos del teclado

--- projects/07-midu-router/src/Link.jsx
+++ projects/07-midu-router/src/Link.jsx
@@ -9,9 +9,16 @@ export function navigate(href){
 }
 
 export function Link({target, to, ...props}){
+
     const handleClick = (event) => {
-        event.preventDefault()
-        navigate(to)
+        const isMainEvent = event.button == 0 // comprueba que sea el botón primario
+        const isModifiedEvent = event.metaKey || event.altKey || event.ctrlKey || event.shiftKey // Verifica si se están presionando teclas modificadoras
+        const isManageableEvent = target == undefined || target == 'self' // Verifica si el enlace se manejará dentro de la misma ventana
+
+        if(isMainEvent && isManageableEvent && !isModifiedEvent){
+            event.preventDefault()
+            navigate(to)
+        }
     }
 
     return <a onClick={handleClick} href={to} target={target} {...props}></a>
