e9068c0215b102a8c7bb804fcb4f8906f2ff2f52
Carlos Tabares <carlosdanieltc@gmail.com>
2024-04-29 12:28:02 +0200

    07. Inicio enrutamiento básico

--- projects/07-midu-router/src/App.jsx
+++ projects/07-midu-router/src/App.jsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const NAVIGATION_EVENT = 'pushstate'
 
+function navigate(href){
+  window.history.pushState({},'',href)
+  //crear evento personalizado para avisar que hemos cambiado la url
+  const navigationEvent = new Event(NAVIGATION_EVENT)
+
+  window.dispatchEvent(navigationEvent)
+}
+
+function HomePage(){
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Home Page</h1>
+      <p>Esto es el ejercicio 07 de React!</p>
+      <button onClick={() => navigate('/about')}>Ir a sobre nosotros</button>
     </>
+  )
+}
+
+function AboutPage(){
+  return (
+    <>
+      <h1>About</h1>
+      <div>
+        <img src="https://media.licdn.com/dms/image/D4D03AQGD-Ukdl5UKDQ/profile-displayphoto-shrink_200_200/0/1688576569361?e=2147483647&v=beta&t=PXRYGlMXzOLK1coNQURn5-hQUFydnSiZ7Y2_QTnsN_8" alt="Foto de Carlos" />
+        <p>Hola! Me llamo Carlos Tabares</p>
+      </div>
+      <button onClick={() => navigate('/')}>Ir a la home</button>
+    </>
+  )
+}
+
+function App() {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+    }
+  },[])
+
+  return (
+    <main>
+      {currentPath == '/' && <HomePage/>}
+      {currentPath == '/about' && <AboutPage/>}
+    </main>
   )
 }
 
