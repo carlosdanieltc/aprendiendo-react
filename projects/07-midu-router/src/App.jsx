06befc6960fe8866b3f3d2d91c5030598c59f9f4
Carlos Tabares <carlosdanieltc@gmail.com>
2024-05-06 11:03:14 +0200

    7. Cración de Componente Route para rutas declarativas

--- projects/07-midu-router/src/App.jsx
+++ projects/07-midu-router/src/App.jsx
@@ -4,6 +4,7 @@ import AboutPage from './pages/About'
 import { Router } from './Router'
 import Page404 from './pages/404'
 import SearchPage from './pages/Search'
+import { Route } from './Route'
 
 const routes = [
   {
@@ -19,14 +20,22 @@ const routes = [
     Component: SearchPage
   }
 ]
+const appRoutes = [
+  {
+    path: '/search/:query',
+    Component: SearchPage
+  }
+]
 
 
 function App() {
   
-
   return (
     <main>
-      <Router routes={routes} defaultComponent={Page404}></Router>
+      <Router routes={appRoutes} defaultComponent={Page404}>
+        <Route path='/' Component={HomePage}></Route>
+        <Route path='/about' Component={AboutPage}></Route>
+      </Router>
     </main>
   )
 }
