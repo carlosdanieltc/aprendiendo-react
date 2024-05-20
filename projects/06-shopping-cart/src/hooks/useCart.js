892e51d71c439c4428a7f11d98f51e3b3de18b0b
Carlos Tabares <carlosdanieltc@gmail.com>
2024-05-20 14:33:43 +0200

    .Correciones

--- projects/06-shopping-cart/src/hooks/useCart.js
+++ projects/06-shopping-cart/src/hooks/useCart.js
@@ -1,12 +1,12 @@
-import { useContext } from "react";
-import { CartContext } from "../context/cart";
+import { useContext } from 'react'
+import { CartContext } from '../context/cart.jsx'
 
 export const useCart = () => {
-    const context = useContext(CartContext)
+  const context = useContext(CartContext)
 
-    if(context === undefined) {
-        throw new Error('useCart must be used within a CartProvider')
-    }
+  if (context === undefined) {
+    throw new Error('useCart must be used within a CartProvider')
+  }
 
-    return context
+  return context
 }
